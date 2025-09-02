0cad574b741f50c2e6232ac44112c8394375cac1
Alexis <josephcuevasrafael@gmail.com>
2025-09-02 15:30:39 -0500

    Subiendo ejercicio - SEMANA 1

---
+++ SEMANA3_C#/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA3_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+        static void ejer1() 
+        {
+            //DECLARAMOS VARIABLES
+            string nombre, carrera;
+
+            //MOSTRAR MENSAJE
+            Console.Write("Ingrese su nombnre:");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su carrera:");
+            carrera = Console.ReadLine();
+
+            //MOSTRAR RESULTADO
+            Console.WriteLine($"\n{nombre}, Bienvenido al curso de Fundamentos de Algoritmos de la carrera {carrera}");
+
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ SEMANA3_PYTHON/SEMANA3_PYTHON.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre = input("(P)Ingrese su nombre:")
+    carrera = input("(P)Ingrese su carrera:")
+
+    print(f"\n{nombre}, Bienvenido al curso de Fundamentos de Algoritmos de la carrera {carrera}")
+
+ejer1()
