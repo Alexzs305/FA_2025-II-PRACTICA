0ded8f26e3c685abe2f7121ea6499ef4a2512cab
James <adrianorruiz.25@gmail.com>
2025-09-09 15:38:47 -0500

    ejer5

--- SEMANA3_C#/Program.cs
+++ SEMANA3_C#/Program.cs
@@ -10,8 +10,7 @@ namespace SEMANA3_C_
     {
         static void Main(string[] args)
         {
-            ejer1();
-            Console.ReadKey();
+            ejer5();
         }
         static void ejer1() 
         {
@@ -32,23 +31,47 @@ namespace SEMANA3_C_
         static void ejer2()
         {
             Console.WriteLine("Ingrese un numero x: ");
-            int x = int.Parse(console.readline());
+            int x = int.Parse(Console.ReadLine());
 
-            console.writeline("Ingrese un numero y: ");
-            int y = Convert.ToInt32(console.readline());
+            Console.WriteLine("Ingrese un numero y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
 
-            double reesultado = 
+            double reesultado = (double)x / (double)y;
+
+            Console.WriteLine("suma: "+(x+y));
+            Console.WriteLine("resta: "+(x-y));
+            Console.WriteLine("multiplicacion: "+(x*y));
+            Console.WriteLine("division: "+(x/y));
         }
         static void ejer3()
         {
-
+            Console.WriteLine("\"ALEXIS\"");
         }
         static void ejer4()
         {
+            Console.WriteLine("Ingrese un numero decimal: ");
+            double num = Convert.ToDouble(Console.ReadLine());
 
+            double raiz2= Math.Sqrt(num);
+            int redo = (int)Math.Round(num);
+            double cubo = Math.Pow(num, 3);
+            double raiz3 = Math.Pow(num, 1 / 3d);
+
+            Console.WriteLine("Raiz2" + raiz2);
+            Console.WriteLine("Redondeo: " + redo);
+            Console.WriteLine("Cubo: " + cubo); 
+            Console.WriteLine("Raiz3: " + raiz3);
         }
         static void ejer5()
         {
+            Console.Write("Ingrese un numnero: ");
+            string num = Console.ReadLine();
+
+            int entero = int.Parse(num);
+            double deci = double.Parse(num);
+
+            Console.WriteLine("Resto: "+(entero%2));
+            Console.WriteLine("Division" + (deci / 3));
 
         }
         static void ejer6()
--- SEMANA3_PYTHON/SEMANA3_PYTHON.py
+++ SEMANA3_PYTHON/SEMANA3_PYTHON.py
@@ -5,6 +5,8 @@ def ejer1():
     print(f"\n{nombre}, Bienvenido al curso de Fundamentos de Algoritmos de la carrera {carrera}")
 
 def ejer2():
+    print("\"\"ALEXIS\"\"")
+def ejer3():
     x= int(input("Ingrese el valor de x:"))
     y= int(input("Ingrese el valor de y:"))
 
@@ -12,5 +14,25 @@ def ejer2():
     print("resta", (x-y))
     print("Multiplicación", (x*y))
     print("Division", (x/y))
-    
-    ejer2()
+
+import math #IMPORTANDO LA LIBRERIA MATH
+
+def ejer4():
+    num= int(input("Ingrese un numero decimal: "))
+
+    print ("Raiz2: ", math.sqrt(num))
+    print ("Redondeado: ", round(num, 0))
+    print("Al cubo: ", math.pow(num, 3))
+    print("raiz3: ", num**(1/3))
+
+def ejer5():
+    num = input("Ingrese un numero: ")
+
+    entero = int(num)
+    deci = float(num)
+
+    print("Resto: ", (entero%2))
+    print("Division: ", (deci/3))
+
+ejer5()
+ 
